9629b926b0f8e4b04aac15cc5cb317aae33e4379
Josh Hodgson <josh.pbh@gmail.com>
2021-12-01 21:29:49 +0000

    even more shorter

--- day-01/short.py
+++ day-01/short.py
@@ -1,3 +1,3 @@
-values = [int(f.strip("\n")) for f in open("example.txt")]
-print(sum([1 for x in range(1, len(values)) if values[x] > values[x-1]]))
-print(sum([1 for x in range(3, len(values)) if sum(values[x-2:x+1]) > sum(values[x-3:x])]))
+values = [int(f.strip("\n")) for f in open("input.txt")]
+print(sum(values[x] > values[x-1] for x in range(1, len(values))))
+print(sum(sum(values[x-2:x+1]) > sum(values[x-3:x]) for x in range(3, len(values))))
