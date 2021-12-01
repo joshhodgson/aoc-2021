34b31ffe89683b6a924914a4b724a4f0e74976f5
Josh Hodgson <josh.pbh@gmail.com>
2021-12-01 21:26:26 +0000

    even shorter

--- day-01/short.py
+++ day-01/short.py
@@ -1,4 +1,3 @@
 values = [int(f.strip("\n")) for f in open("example.txt")]
 print(sum([1 for x in range(1, len(values)) if values[x] > values[x-1]]))
-groupings = [sum(values[x-2:x+1]) for x in range(2, len(values))]
-print(sum([1 for x in range(1, len(groupings)) if groupings[x] > groupings[x-1]]))
+print(sum([1 for x in range(3, len(values)) if sum(values[x-2:x+1]) > sum(values[x-3:x])]))
