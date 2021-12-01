17506606c6cd5acecf3f3b0fbcc4dc91a7675111
Josh Hodgson <josh.pbh@gmail.com>
2021-12-01 21:15:41 +0000

    formatting

--- day-01/part1.py
+++ day-01/part1.py
@@ -1,6 +1,6 @@
 values = [int(f.strip("\n")) for f in open("input.txt")]
-total=0
-for i in range(1,len(values)):
-    if values[i]>values[i-1]:
-        total+=1
+total = 0
+for i in range(1, len(values)):
+    if values[i] > values[i-1]:
+        total += 1
 print(total)
--- day-01/part2.py
+++ day-01/part2.py
@@ -1,9 +1,9 @@
 values = [int(f.strip("\n")) for f in open("input.txt")]
-ranges=[]
-for i in range(2,len(values)):
+ranges = []
+for i in range(2, len(values)):
     ranges.append(values[i]+values[i-1]+values[i-2])
-total=0
-for i in range(1,len(ranges)):
-    if ranges[i]>ranges[i-1]:
-        total+=1
+total = 0
+for i in range(1, len(ranges)):
+    if ranges[i] > ranges[i-1]:
+        total += 1
 print(total)
