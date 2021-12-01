db7fd7caa36470dcfbf1810400ba541acb714cf1
Josh Hodgson <josh.pbh@gmail.com>
2021-12-01 21:31:06 +0000

    formatting

--- day-01/short.py
+++ day-01/short.py
@@ -1,3 +1,5 @@
 values = [int(f.strip("\n")) for f in open("input.txt")]
-print(sum(values[x] > values[x-1] for x in range(1, len(values))))
-print(sum(sum(values[x-2:x+1]) > sum(values[x-3:x]) for x in range(3, len(values))))
+print(
+    sum(values[x] > values[x-1] for x in range(1, len(values))),
+    sum(sum(values[x-2:x+1]) > sum(values[x-3:x]) for x in range(3, len(values)))
+)
